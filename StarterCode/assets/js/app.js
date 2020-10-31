b5d6e275e1d8afb83e4623641d3e71d66d29394d
ssugiura47 <67213943+ssugiura47@users.noreply.github.com>
2020-10-31 07:32:10 -0700

    finalizing

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -2,17 +2,6 @@ function unpack(rows, index){
     return rows.map(row => row[index])
 };
 
-function getData(){
-    d3.csv("assets/data/data.csv").then(function(data) {
-        var poverty = data.map(d => d.poverty);
-        var healthcare = data.map(d => d.healthcare);
-        var state = data.map(d => d.state);
-        poverty = +poverty;
-        healthcare = +healthcare;
-        state = +state;
-    })
-};
-
 function init(){
     d3.csv("assets/data/data.csv").then(function(data) {
     var svgWidth = 960;
@@ -37,32 +26,20 @@ function init(){
     var chartGroup = svg.append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    getData();
-
-    // console.log(poverty)
+    // Format the date and cast the force value to a number
+    data.forEach(function(d) {
+      d.poverty = +d.poverty
+      d.healthcare = +d.healthcare
+    });
+    
     // Create scale functions
     var xLinearScale = d3.scaleLinear()
-      .domain([0, 25])
+      .domain(d3.extent(data, d => d.poverty))
       .range([0, width]);
-
-    var yLinearScale = d3.scaleLinear()
-      .domain([0, 30])
-      .range([height, 0]);
-
-    // Format the date and cast the force value to a number
-    // data.forEach(function(d) {
-    //   d.poverty = +d.poverty
-    //   d.healtcare = +d.healtcare
-    // });
-    
-    // // Create scale functions
-    // var xLinearScale = d3.scaleLinear()
-    //   .domain(d3.extent(data, d => +d.poverty))
-    //   .range([0, width]);
       
-    // var yLinearScale = d3.scaleLinear()
-    //   .domain([0, d3.max(data, d => +d.healthcare)])
-    //   .range([height,0]);
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.healthcare)+5])
+      .range([height,0]);
 
     // Create axis functions
     var bottomAxis = d3.axisBottom(xLinearScale);
@@ -71,10 +48,10 @@ function init(){
     // Append Axes to the chart
     chartGroup.append("g")
     .attr("transform", `translate(0, ${height})`)
-     .call(bottomAxis);
+    .call(bottomAxis);
 
     chartGroup.append("g")
-     .call(leftAxis);
+    .call(leftAxis);
 
     // Create Circles
     var circlesGroup = chartGroup.selectAll("circle")
@@ -86,6 +63,8 @@ function init(){
     .attr("r", "15")
     .attr("opacity", ".5")
     .classed("stateCircle", true)
+
+    console.log(data)
 
     var circleText = chartGroup.selectAll("stateText")
     .data(data)
@@ -102,7 +81,7 @@ function init(){
       .attr("class", "tooltip")
       .offset([80, -60])
       .html(function(d) {
-        return (`${d.state}<br>Poverty: ${d.poverty}%<br>Healthcare: ${d.healtcare}%`);
+        return (`${d.state}<br>Poverty: ${d.poverty}%<br>Healthcare: ${d.healthcare}%`);
       });
 
     // Create tooltip in the chart
@@ -121,7 +100,7 @@ function init(){
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2) - 45)
+      .attr("x", 0 - (height / 2) - 80)
       .attr("dy", "1em")
       .attr("class", "axisText")
       .text("Lack of Healthcare (%)");
